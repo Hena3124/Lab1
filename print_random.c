f4988cb1a195134c5b6922b2f221912bb0d6eb04
Hena3124 <hena.patel0002@temple.edu>
2024-08-31 23:54:05 -0400

    third

--- print_random.c
+++ print_random.c
@@ -1,31 +1,50 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "random.c"
+
+// void rand_string(char *str, size_t size) {
+//     for (size_t i = 0; i < size; i++) {
+//         str[i] = randchar();
+//     }
+//     str[size] = '\0';
+// }
+
+// char* rand_string_alloc(size_t size) {
+//     char *s = malloc(size + 1);
+//     if (s) {
+//         rand_string(s, size);
+//     }
+//     return s;
+// }
+
+// int main() {
+//     srand(time(NULL)); 
+
+//     char *random_word = rand_string_alloc(7); // Generateing a 7-letter word here
+//     if (random_word) {
+//         printf("Random 7-letter word: %s\n", random_word);
+//         free(random_word); 
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "random.c"
+#include <time.h>
+#include "random.h"
 
-void rand_string(char *str, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        str[i] = randchar();
-    }
-    str[size] = '\0';
+int main()
+{
+	int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
-
-char* rand_string_alloc(size_t size) {
-    char *s = malloc(size + 1);
-    if (s) {
-        rand_string(s, size);
-    }
-    return s;
-}
-
-int main() {
-    srand(time(NULL)); 
-
-    char *random_word = rand_string_alloc(7); // Generateing a 7-letter word here
-    if (random_word) {
-        printf("Random 7-letter word: %s\n", random_word);
-        free(random_word); 
-    }
-
-    return 0;
-}
-
